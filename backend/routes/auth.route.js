39ff3f2bbfb2aa13eec9c8372fe2cb843520a527
Sagar Raut <sagarraut2059@gmail.com>
2024-12-03 18:48:18 +0545

    MPE

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, register } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
 router.post("/register", register);
